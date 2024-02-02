0d400ed75953c61378a4a1023b691b07c890fb74
Margarida Costa <143644045+MargaridaAMGC@users.noreply.github.com>
2024-02-02 19:11:39 +0000

    AULA 1 E 2 (incompleta)

---
+++ app.js
@@ -0,0 +1,11 @@
+function exibirTextoNaTela(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+
+}
+exibirTextoNaTela('h1','Jogo do Número Secreto');
+exibirTextoNaTela('p', 'Escolha um número entre 1 e 10!');
+
+function verificarChute() {
+    console.log('O botão foi clicado');
+}
